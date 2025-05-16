b5b5478490ac0cde86fce5e6d6b0af27d955512a
UrPrice <denis.korneevbk@gmail.com>
2025-05-16 20:29:55 +0300

    post add

--- jsData/traders.js
+++ jsData/traders.js
@@ -510,6 +510,57 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }
         ]
     });
+    addTrader(1072 , 1923, {
+        name: "Барахольщик | Приграничный пост",
+        quests: [
+            {
+                id: "trader_candelabra",
+                level: 40,
+                rewards: "20 опыта"
+            },
+            {
+                id: "trader_butter_knife",
+                level: 40,
+                rewards: "20 опыта"
+            },
+            {
+                id: "trader_inkwell",
+                level: 40,
+                rewards: "20 опыта",
+                notes: "чернильницу можно купить у этого же трейдера"
+            },
+            { 
+                id: "trader_blades", 
+                level: 40, 
+                rewards: "20 опыта"
+            },
+            { 
+                id: "trader_glasses", 
+                level: 40, 
+                rewards: "20 опыта"
+            },
+            {
+                id: "trader_collection",
+                level: 40,
+                rewards: "20 опыта"
+            },
+            {
+                id: "trader_smoking",
+                level: 40,
+                rewards: "20 опыта"
+            },
+            {
+                id: "trader_seashell",
+                level: 40,
+                rewards: "20 опыта"
+            },
+            {
+                id: "trader_perfume",
+                level: 40,
+                rewards: "20 опыта"
+            }
+        ]
+    });
     addTrader(1636 , 2255, {
         name: "Барахольщик | Деревня Карстен",
         quests: [

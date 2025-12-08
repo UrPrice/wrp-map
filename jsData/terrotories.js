582f4ea862f25294c0cd32693f5804469ee92b97
UrPrice <denis.korneevbk@gmail.com>
2025-12-08 16:31:24 +0300

    updating-content

--- jsData/terrotories.js
+++ jsData/terrotories.js
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     addTerritory(1625 , 2249, 30, 'Temeria', 'Деревня Карстен');
     addTerritory(673 , 1292, 90, 'Baron', 'Вроницы');
     addTerritory(1398 , 1805, 30, 'Redania', 'Каменоломня Старые Хрычи');
-    addTerritory(465 , 1582, 55, 'Bandits', 'Деревня Вересковка');
-    addTerritory(57 , 967, 30, 'Baron', 'Лесопилка Брука');
-    addTerritory(2818 , 664, 60, 'Bandits', 'Мельница Белых Садов');
-    addTerritory(847 , 1165, 40, 'Baron', 'Лагерь поддержки');
+    addTerritory(465 , 1582, 55, 'Baron', 'Деревня Вересковка');
+    addTerritory(57 , 967, 30, 'Nilf', 'Лесопилка Брука');
+    addTerritory(2818 , 664, 60, 'Temeria', 'Мельница Белых Садов');
+    addTerritory(847 , 1165, 40, 'Redania', 'Лагерь поддержки');
     addTerritory(1870 , 136, 120, 'Nilf', 'Вызима');
-    addTerritory(1171 , 1620, 40, 'Redania', 'Деревня Яворник');
+    addTerritory(1171 , 1620, 40, 'Bandits', 'Деревня Яворник');
     addTerritory(1055 , 1941, 45, 'Redania', 'Приграничный пост');
     addTerritory(1558 , 1080, 30, 'Redania', 'Поместье Реордан');
     addTerritory(1083 , 1255, 50, 'Redania', 'Деревня Залипье');
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     addTerritory(2242 , 524, 80, 'Nilf', 'Деревня Ольшина');
     addTerritory(2119 , 581, 30, 'Nilf', 'Деревня Ольшина');
     addTerritory(2550 , 750, 30, 'Nilf', 'Руины замка Реда');
-    addTerritory(2234 , 2970, 140, 'Temeria', 'Залесье (Caer Middore)');
+    addTerritory(2234 , 2970, 140, 'Temeria', 'Залесье');
     addTerritory(3113 , 416, 50, 'Neutral', 'Сгоревшая деревня');
     addTerritory(1775 , 1247, 40, 'Neutral', 'Деревня Тодерас');
     addTerritory(324 , 400, 40, 'Neutral', 'Деревня Хель');
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     addTerritory(1922 , 2939, 25, 'Neutral', 'Ферма');
     addTerritory(1914 , 2855, 25, 'Neutral', 'Хутор Фолье');
     addTerritory(1862 , 3022, 50, 'Temeria', 'Деревня Лесьна');
-    addTerritory(1861 , 2755, 50, 'Bandits', 'Хозяйство Фолье');
+    addTerritory(1861 , 2755, 50, 'Temeria', 'Хозяйство Фолье');
     addTerritory(2435 , 2645, 40, 'Temeria', 'Наследие Предков');
 
 
